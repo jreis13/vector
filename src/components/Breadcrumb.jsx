5397c71c05d869b12f0880f3584d9af4e8458af8
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-06-24 20:08:46 +0100

    update Breadcrumb

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -1,8 +1,37 @@
 "use client";
 
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 function Breadcrumb({ sections }) {
+  const [currentSection, setCurrentSection] = useState("");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let foundSection = "";
+      sections.forEach((section) => {
+        const element = document.getElementById(section.id);
+        if (element) {
+          const rect = element.getBoundingClientRect();
+          if (
+            rect.top <= window.innerHeight / 2 &&
+            rect.bottom >= window.innerHeight / 2
+          ) {
+            foundSection = section.id;
+          }
+        }
+      });
+      setCurrentSection(foundSection);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [sections]);
+
   const scrollToSection = (id) => {
     if (id === "") {
       window.scrollTo({ top: 0, behavior: "smooth" });
@@ -16,12 +45,15 @@ function Breadcrumb({ sections }) {
 
   return (
     <nav className="fixed bottom-0 right-0 mx-6 mb-6 lg:mx-8 lg:mb-8 z-10 flex flex-col-reverse">
-      <ol className="list-none flex flex-col space-y-6 ">
-        {sections.map((section, index) => (
-          <li key={section.title} className="flex items-center ">
-            {index !== 0}
+      <ol className="list-none flex flex-col space-y-6">
+        {sections.map((section) => (
+          <li key={section.title} className="flex items-center">
             <button
-              className=" flex items-center space-x-1"
+              className={`flex items-center space-x-1 ${
+                currentSection === section.id
+                  ? "text-indigo-600"
+                  : "text-gray-400"
+              }`}
               onClick={() => scrollToSection(section.id)}
             >
               <Image
@@ -29,6 +61,9 @@ function Breadcrumb({ sections }) {
                 alt={section.title}
                 width={24}
                 height={24}
+                className={`${
+                  currentSection === section.id ? "filter-blue" : ""
+                }`}
               />
             </button>
           </li>
