dad9ee71cdb42f4754ac2eb43cea59db40dae34b
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-06-24 20:07:18 +0100

    update data

--- src/common/data/breadcrumbData.jsx
+++ src/common/data/breadcrumbData.jsx
@@ -2,10 +2,12 @@ import AnalysisIcon from "/public/analysis.svg";
 import BenefitsIcon from "/public/benefits.svg";
 import HomeIcon from "/public/home.svg";
 import OfferingIcon from "/public/offering.svg";
+import subscribeIcon from "/public/subscribeIcon.svg";
 
 export const breadcrumbSections = [
   { title: "Home", icon: HomeIcon, id: "" },
   { title: "Benefits", icon: BenefitsIcon, id: "benefits" },
   { title: "Offering", icon: OfferingIcon, id: "offering" },
   { title: "Analysis", icon: AnalysisIcon, id: "analysis" },
+  { title: "subscribe", icon: subscribeIcon, id: "subscribe" },
 ];
