fc28fea78071444bd313fe64a6d3892d9eff5809
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-06-24 16:52:53 +0100

    extract data

---
+++ src/common/data/breadcrumbData.jsx
@@ -0,0 +1,11 @@
+import AnalysisIcon from "/public/analysis.svg";
+import BenefitsIcon from "/public/benefits.svg";
+import HomeIcon from "/public/home.svg";
+import OfferingIcon from "/public/offering.svg";
+
+export const breadcrumbSections = [
+  { title: "Home", icon: HomeIcon, id: "" },
+  { title: "Benefits", icon: BenefitsIcon, id: "benefits" },
+  { title: "Offering", icon: OfferingIcon, id: "offering" },
+  { title: "Analysis", icon: AnalysisIcon, id: "analysis" },
+];
---
+++ src/common/data/featureData.jsx
@@ -0,0 +1,78 @@
+import arcLogo from "/public/arcLogo.png";
+import beOneStepAheadIcon from "/public/beOneStepAheadIcon.svg";
+import companyResearchIcon from "/public/companyResearchIcon.svg";
+import krooLogo from "/public/krooLogo.svg";
+import privateEquityIcon from "/public/privateEquityIcon.svg";
+import stratiphyLogo from "/public/stratiphyLogo.svg";
+
+export const benefitsFeatures = [
+  {
+    name: "Company Research Analysis",
+    description:
+      "Get weekly in-depth analysis on companies launching their equity crowdfunding campaigns.",
+    image: companyResearchIcon,
+  },
+  {
+    name: "Private Equity Data",
+    description:
+      "Get permanent exclusive access to our database of private companies.",
+    image: privateEquityIcon,
+  },
+  {
+    name: "Be One Step Ahead",
+    description:
+      "Keep up to date with news of equity crowdfunding rounds and trends in the various prominent industries.",
+    image: beOneStepAheadIcon,
+  },
+];
+
+export const offeringFeatures = [
+  {
+    name: "Company Research",
+    description:
+      "Lorem ipsum dolor sit amet. Sit exercitationem pariatur et dolores perferendis eos unde tenetur non expedita obcaecati quo dolore..",
+  },
+  {
+    name: "Company Data",
+    description:
+      "Ut debitis blanditiis non tempore rerum ut explicabo dicta ut quisquam voluptas 33 optio alias ut obcaecati..",
+  },
+  {
+    name: "Ecosystem Updates",
+    description:
+      "Qui fugit velit et quos veritatis qui maiores quas vel volupta tes voluptatibus non expedita obcaecati quo dolore.",
+  },
+  {
+    name: "Platform Comparison",
+    description:
+      "Lorem ipsum dolor sit amet. Sit exercitationem pariatur et dolores perferendis eos unde tenetur non expedita obcaecati quo dolore.",
+  },
+  {
+    name: "Weekly Newsletter",
+    description:
+      "Ut debitis blanditiis non tempore rerum ut explicabo dicta ut quisquam voluptas 33 optio alias ut obcaecati..",
+  },
+  {
+    name: "On-demand Research",
+    description:
+      "Qui fugit velit et quos veritatis qui maiores quas vel volupta tes voluptatibus non expedita obcaecati quo dolore.",
+  },
+];
+
+export const analysisFeatures = [
+  {
+    description:
+      "Lorem ipsum dolor sit amet. Sit exercitationem pariatur et dolores perferendis eos unde tenetur non expedita obcaecati quo dolore",
+    image: krooLogo,
+  },
+  {
+    description:
+      "Ut debitis blanditiis non tempore rerum ut explicabo dicta ut quisquam voluptas 33 optio alias ut obcaecati.",
+    image: arcLogo,
+  },
+  {
+    description:
+      "Qui fugit velit et quos veritatis qui maiores quas vel voluptates voluptatibus.",
+    image: stratiphyLogo,
+  },
+];
