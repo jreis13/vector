fb450103f32473a8de455689ef285613331cad1b
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-06-27 15:21:35 +0100

    remove useless property

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ function Button({ onClick, href, children }) {
   if (onClick) {
     return (
       <button
-        className="inline-flex justify-center w-full max-w-xs py-4 border border-white rounded-full cursor-pointer uppercase transition-colors duration-200 hover:font-medium hover:bg-[#BB44F0]"
+        className="inline-flex justify-center w-full max-w-xs py-4 border rounded-full cursor-pointer uppercase transition-colors duration-200 hover:font-medium hover:bg-[#BB44F0]"
         onClick={onClick}
         type="button"
       >
