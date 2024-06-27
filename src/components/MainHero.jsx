424e921d370cfc392710615db3f9fdfa08671ee2
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-06-27 15:21:19 +0100

    change mainHero image

--- src/components/MainHero.jsx
+++ src/components/MainHero.jsx
@@ -6,14 +6,21 @@ function MainHero({ children }) {
     <div className="main-hero relative overflow-hidden flex justify-center items-center pt-24">
       <div className="absolute inset-0 w-full h-full z-[-1]">
         <Image
-          src="/vector.png"
+          src="/mainHero.svg"
           alt="Background image"
           fill
           style={{ objectPosition: "center" }}
         />
       </div>
       <div className="relative z-10 text-center py-6">
-        {children}
+        <div
+          style={{
+            textShadow:
+              "1px 1px 1px #4d405e, 0px 0px 1px #4d405e, 1px 0px 1px #4d405e, 0px 1px 1px #4d405e",
+          }}
+        >
+          {children}
+        </div>
         <div className="my-6">
           <Button href="mailto:contact@vector.com">Join Us</Button>
         </div>
