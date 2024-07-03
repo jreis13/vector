8c753ca90b1b901bb8082867377840f415ed2705
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-07-03 10:03:39 +0100

    home page meta data

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,15 @@
-import "../common/styles/globals.css";
+import "../common/styles/globals.css"
 
 export const metadata = {
   title: "Vector",
-  description: "A description of my Next.js app",
-};
+  description:
+    "Unlock equity crowdfunding analysis and real private equity data delivered directly to your inbox.",
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import "../common/styles/_reset.css";
+import "../common/styles/_reset.css"
 
-import MainLayout from "src/layouts/MainLayout";
+import MainLayout from "src/layouts/MainLayout"
 
 export default function Home() {
   return (
     <div>
       <MainLayout />
     </div>
-  );
+  )
 }
