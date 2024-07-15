081242213de27a87d64531e3aac65f2da33f4d6f
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-07-15 20:22:12 +0100

    fix foundingTeam

--- src/components/Company/CompanyFoundingTeam.jsx
+++ src/components/Company/CompanyFoundingTeam.jsx
@@ -10,27 +10,28 @@ function CompanyFoundingTeam({ foundingTeam }) {
       <h2 className="pb-4 text-lg font-bold">Founding Team</h2>
       <div className="grid grid-cols-1 gap-4 pb-8 md:grid-cols-2 lg:grid-cols-3">
         {foundingTeam.map((member, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center rounded-lg p-4"
-          >
-            <div className="group relative">
-              <Image
-                src={member.photo ? member.photo : placeholder}
-                alt={`${member.label} photo`}
-                width={100}
-                height={100}
-                className="rounded-full"
-              />
-              <div className="absolute inset-0 flex items-center justify-center rounded-full bg-black bg-opacity-75 text-white opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
-                <Link blank to={member.linkedin}>
-                  <FontAwesomeIcon icon={faLinkedin} className="text-2xl" />
-                </Link>
+          <div key={index} className="group p-4">
+            <div className="relative flex flex-col items-center p-4">
+              <div className="mb-4">
+                <Image
+                  src={member.photo ? member.photo : placeholder}
+                  alt={`${member.label} photo`}
+                  width={100}
+                  height={100}
+                  className="rounded-full"
+                />
               </div>
-            </div>
-            <div className="mt-4 text-center">
-              <span className="font-semibold">{member.label}</span>
-              <p>{member.value}</p>
+              <div className="mt-4 text-center">
+                <span className="font-semibold">{member.label}</span>
+                <p>{member.value}</p>
+              </div>
+              {member.linkedin && (
+                <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-75 text-white opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
+                  <Link blank to={member.linkedin}>
+                    <FontAwesomeIcon icon={faLinkedin} className="text-2xl" />
+                  </Link>
+                </div>
+              )}
             </div>
           </div>
         ))}
