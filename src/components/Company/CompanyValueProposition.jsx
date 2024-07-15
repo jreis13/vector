21755838e9f71c9c32c7b6743542b05dca7e8470
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-07-15 20:06:48 +0100

    update valueProposition

--- src/components/Company/CompanyValueProposition.jsx
+++ src/components/Company/CompanyValueProposition.jsx
@@ -2,7 +2,16 @@ function CompanyValueProposition({ valueProposition }) {
   return (
     <div className="flex min-h-screen flex-col py-8 lg:py-16">
       <h2 className="pb-4 text-lg font-bold">Value Proposition</h2>
-      <p className="pb-8">{valueProposition}</p>
+      <div className="pb-8 leading-8">
+        {valueProposition
+          .split(".")
+          .filter((sentence) => sentence.trim() !== "")
+          .map((sentence, index) => (
+            <p key={index} className="mb-4">
+              {sentence.trim()}.
+            </p>
+          ))}
+      </div>
     </div>
   )
 }
