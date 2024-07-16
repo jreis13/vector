718e18cfb4e2e73338ece19e9660c7439108685c
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-07-16 19:31:36 +0100

    update competitors

--- src/components/Company/CompanyKeyCompetitors.jsx
+++ src/components/Company/CompanyKeyCompetitors.jsx
@@ -2,14 +2,14 @@ import CompetitorCard from "./CompetitorCard"
 
 function CompanyKeyCompetitors({ keyCompetitors }) {
   return (
-    <>
+    <div className="flex flex-col py-8 lg:py-16">
       <h2 className="pb-4 text-lg font-bold">Key Competitors</h2>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-4 pb-8 md:grid-cols-2 lg:grid-cols-3">
         {keyCompetitors.map((competitor, index) => (
           <CompetitorCard key={index} competitor={competitor} />
         ))}
       </div>
-    </>
+    </div>
   )
 }
 
--- src/components/Company/CompetitorCard.jsx
+++ src/components/Company/CompetitorCard.jsx
@@ -1,42 +1,50 @@
-"use client"
-
-import { useRouter } from "next/navigation"
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Image from "next/image"
+import Link from "../Link"
 
 function CompetitorCard({ competitor }) {
-  const router = useRouter()
-
   const handleCardClick = () => {
     window.open(competitor.website, "_blank")
   }
 
   return (
     <div
-      className="mb-6 flex h-full cursor-pointer flex-col justify-between rounded-lg border p-4 transition-all duration-300 hover:bg-[#e8e8e8] hover:text-[#403f4c] hover:shadow-lg"
+      className="group relative flex flex-col items-center rounded-lg p-4 transition-shadow duration-300 ease-in-out hover:shadow-lg"
       onClick={handleCardClick}
     >
-      <div>
-        <div className="mb-4 flex items-center">
-          <div>
-            <h3 className="text-xl font-bold">{competitor.name}</h3>
-          </div>
-        </div>
-        <div className="mb-4">
-          <p className="text-sm">
+      <div className="relative mb-4 h-24 w-24">
+        <Image
+          src={competitor.logo}
+          alt={`${competitor.name} logo`}
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
+      <div className="text-center">
+        <h3 className="text-xl font-bold">{competitor.name}</h3>
+        <div className="mt-2 text-sm">
+          <p>
             <strong>Year Founded:</strong> {competitor.yearFounded}
           </p>
-          <p className="text-sm">
+          <p>
             <strong>HQ:</strong> {competitor.HQ}
           </p>
-          <p className="text-sm">
+          <p>
             <strong># of Employees:</strong> {competitor.employees}
           </p>
-          <p className="text-sm">
-            <strong>Funding Amount (est):</strong> {competitor.funding}
+          <p>
+            <strong>Funding Amount:</strong> {competitor.funding}
           </p>
-          <p className="text-sm">
+          <p>
             <strong># of Products:</strong> {competitor.products}
           </p>
         </div>
+      </div>
+      <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-75 text-white opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
+        <Link blank to={competitor.website}>
+          <FontAwesomeIcon icon={faExternalLinkAlt} />
+        </Link>
       </div>
     </div>
   )
