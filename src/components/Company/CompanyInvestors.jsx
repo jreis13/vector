bfc92bb31bd98848147571251a6be75379aaeff1
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-07-16 19:31:28 +0100

    edit investors

--- src/components/Company/CompanyInvestors.jsx
+++ src/components/Company/CompanyInvestors.jsx
@@ -8,7 +8,7 @@ function CompanyInvestors({ investors }) {
   return (
     <div className="flex min-h-screen flex-col py-8 lg:py-16">
       <h2 className="pb-4 text-lg font-bold">Investors</h2>
-      <div className="grid grid-cols-1 gap-4 pb-8 md:grid-cols-2">
+      <div className="grid grid-cols-1 gap-4 pb-8 md:grid-cols-2 lg:grid-cols-3">
         {investors.map((investor, index) => (
           <div
             key={index}
