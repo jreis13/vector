0433788f3b00859c1f0e74e978bc512f76d8b142
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-07-16 19:07:56 +0100

    update Financials

--- src/components/Company/CompanyFinancials.jsx
+++ src/components/Company/CompanyFinancials.jsx
@@ -1,16 +1,48 @@
+import {
+  faBalanceScale,
+  faBuilding,
+  faFire,
+  faHandHoldingUsd,
+  faPiggyBank,
+} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const iconMapping = {
+  faBuilding,
+  faPiggyBank,
+  faBalanceScale,
+  faHandHoldingUsd,
+  faFire,
+}
+
 function CompanyFinancials({ financials }) {
   return (
-    <>
+    <div className="flex flex-col py-8 lg:py-16">
       <h2 className="pb-4 text-lg font-bold">Financials</h2>
-      <div className="grid grid-cols-1 gap-4 pb-8 md:grid-cols-2">
-        {financials.map((financial, index) => (
-          <div key={index} className="rounded-lg border p-4">
-            <span className="font-semibold">{financial.label}:</span>{" "}
-            {financial.value}
-          </div>
-        ))}
+      <div className="grid grid-cols-1 gap-4 pb-8 md:grid-cols-2 lg:grid-cols-3">
+        {financials.map((financial, index) => {
+          const icon = iconMapping[financial.icon]
+
+          return (
+            <div
+              key={index}
+              className="group relative flex flex-col items-center p-4"
+            >
+              <div className="mb-4 flex h-24 w-24 items-center justify-center">
+                <FontAwesomeIcon
+                  icon={icon}
+                  className="text-4xl text-[#BB44F0]"
+                />
+              </div>
+              <div className="text-center">
+                <h3 className="font-semibold">{financial.label}</h3>
+                <p>{financial.value}</p>
+              </div>
+            </div>
+          )
+        })}
       </div>
-    </>
+    </div>
   )
 }
 
