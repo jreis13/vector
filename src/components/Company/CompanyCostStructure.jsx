ad1e48886ee23f3d232b9e1f7e784f1a30db236c
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-07-16 18:31:58 +0100

    edit costStructure

--- src/components/Company/CompanyCostStructure.jsx
+++ src/components/Company/CompanyCostStructure.jsx
@@ -1,25 +1,38 @@
 function CompanyCostStructure({ costStructure }) {
+  const { fixed, variable, scalability } = costStructure
+  const maxItems = Math.max(fixed.length, variable.length)
+
   return (
     <div className="flex min-h-screen flex-col py-8 lg:py-16">
       <h2 className="pb-4 text-lg font-bold">Cost Structure</h2>
-      <h3 className="pb-2 font-semibold">Fixed Costs</h3>
-      <ul className="list-disc pb-4 pl-8">
-        {costStructure.fixed.map((cost, index) => (
-          <li key={index}>{cost}</li>
-        ))}
-      </ul>
-      <h3 className="pb-2 font-semibold">Variable Costs</h3>
-      <ul className="list-disc pb-4 pl-8">
-        {costStructure.variable.map((cost, index) => (
-          <li key={index}>{cost}</li>
-        ))}
-      </ul>
-      <h3 className="pb-2 font-semibold">Scalability</h3>
-      <ul className="list-disc pb-4 pl-8">
-        {costStructure.scalability.map((scale, index) => (
-          <li key={index}>{scale}</li>
-        ))}
-      </ul>
+      <div className="flex-grow overflow-x-auto">
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th className="px-4 py-2 text-center">Fixed Costs</th>
+              <th className="px-4 py-2 text-center">Variable Costs</th>
+            </tr>
+          </thead>
+          <tbody className="flex-1">
+            {[...Array(maxItems)].map((_, index) => (
+              <tr key={index}>
+                <td className="px-4 py-2 text-center">{fixed[index]}</td>
+                <td className="px-4 py-2 text-center">{variable[index]}</td>
+              </tr>
+            ))}
+            <tr>
+              <th className="px-4 pt-8 text-center" colSpan="2">
+                Scalability
+              </th>
+            </tr>
+            <tr>
+              <td className="px-4 py-4 text-center" colSpan="2">
+                {scalability}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
