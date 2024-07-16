ce13fa4090ef6fd0e81c7a1840240b6a702c6783
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-07-16 19:07:38 +0100

    edit CustomerGrowth

--- src/components/Company/CompanyCustomerGrowth.jsx
+++ src/components/Company/CompanyCustomerGrowth.jsx
@@ -2,7 +2,7 @@ function CompanyCustomerGrowth({ customerGrowth }) {
   return (
     <>
       <h2 className="pb-4 text-lg font-bold">Customer Growth</h2>
-      <div className="grid grid-cols-1 gap-4 pb-8 md:grid-cols-2">
+      <div className="pb-8">
         {customerGrowth.map((growth, index) => (
           <ul key={index} className="list-disc pl-4">
             <li>{growth}</li>
