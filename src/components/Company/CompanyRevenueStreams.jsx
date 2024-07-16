8c6f1fb36f6dad68af1b5377726f3c3ead1514bb
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-07-16 18:31:46 +0100

    edit revenueStreams

--- src/components/Company/CompanyRevenueStreams.jsx
+++ src/components/Company/CompanyRevenueStreams.jsx
@@ -1,14 +1,44 @@
+import {
+  faChalkboardTeacher,
+  faExchangeAlt,
+  faHandshake,
+  faPlane,
+  faTools,
+} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const iconMapping = {
+  faPlane,
+  faExchangeAlt,
+  faTools,
+  faHandshake,
+  faChalkboardTeacher,
+}
+
 function CompanyRevenueStreams({ revenueStreams }) {
   return (
-    <div className="flex min-h-screen flex-col py-8 lg:py-16">
+    <div className="flex flex-col py-8 lg:py-16">
       <h2 className="pb-4 text-lg font-bold">Revenue Streams</h2>
-      <div className="grid grid-cols-1 gap-4 pb-8 md:grid-cols-2">
-        {revenueStreams.map((stream, index) => (
-          <div key={index} className="rounded-lg border p-4">
-            <span className="font-semibold">{stream.label}:</span>{" "}
-            {stream.value}
-          </div>
-        ))}
+      <div className="grid grid-cols-1 gap-4 pb-8 md:grid-cols-2 lg:grid-cols-3">
+        {revenueStreams.map((stream, index) => {
+          const icon = iconMapping[stream.icon]
+          return (
+            <div
+              key={index}
+              className="group relative flex flex-col items-center p-4"
+            >
+              <div className="mb-4 flex h-24 w-24 items-center justify-center">
+                <FontAwesomeIcon
+                  icon={icon}
+                  className="text-4xl text-[#BB44F0]"
+                />
+              </div>
+              <div className="text-center">
+                <p className="font-semibold">{stream.value}</p>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
