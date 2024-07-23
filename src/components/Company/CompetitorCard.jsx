c61a92bb528bf6e0a807eae3e876e428bf3cfeed
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-07-23 15:17:23 +0100

    remove links in competitorCard

--- src/components/Company/CompetitorCard.jsx
+++ src/components/Company/CompetitorCard.jsx
@@ -1,18 +1,8 @@
-import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Image from "next/image"
-import Link from "../Link"
 
 function CompetitorCard({ competitor }) {
-  const handleCardClick = () => {
-    window.open(competitor.website, "_blank")
-  }
-
   return (
-    <div
-      className="group relative flex flex-col items-center rounded-lg p-4 transition-shadow duration-300 ease-in-out hover:shadow-lg"
-      onClick={handleCardClick}
-    >
+    <div className="group relative flex flex-col items-center p-4">
       <div className="relative mb-4 h-24 w-24">
         <Image
           src={competitor.logo}
@@ -40,11 +30,6 @@ function CompetitorCard({ competitor }) {
             <strong># of Products:</strong> {competitor.products}
           </p>
         </div>
-      </div>
-      <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-75 text-white opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
-        <Link blank to={competitor.website}>
-          <FontAwesomeIcon icon={faExternalLinkAlt} />
-        </Link>
       </div>
     </div>
   )
