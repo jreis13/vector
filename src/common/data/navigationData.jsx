679a0885d7eb8537a625dc0a0ba6d4d73249171a
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-11-13 21:41:11 +0000

    remove unused links

--- src/common/data/navigationData.jsx
+++ src/common/data/navigationData.jsx
@@ -1,9 +1,6 @@
 export const PATHS = [
   { name: "Companies", path: "/companies/" },
   { name: "Ecosystem", path: "/ecosystem/" },
-  { name: "Platforms", path: "/platforms/" },
-  { name: "Pricing", path: "/pricing/" },
-  { name: "About", path: "/about/" },
   { name: "Login", path: "/login/" },
 ]
 
