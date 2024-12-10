20c69eff628d59394fdc809656869d1a6d0f557d
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-12-10 19:42:10 +0000

    redo

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -9,6 +9,9 @@ function CTA() {
     <div className="flex min-h-screen flex-col items-center justify-center px-6 py-8 lg:px-16 lg:py-16">
       <div className="grid grid-cols-1 gap-y-16 sm:gap-y-20 lg:grid-cols-2">
         <div className="flex flex-col gap-6 lg:pt-4">
+          <div className="flex flex-col gap-y-6">
+            <h2>Join Us</h2>
+          </div>
           <Button href="mailto:support@exponentialvector.eu">Join Us</Button>
         </div>
         <Image
