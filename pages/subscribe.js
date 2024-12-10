69cf0b32dd68af344fb43b338e1fc2c6d6883119
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-12-10 22:48:35 +0000

    fix route

--- pages/subscribe.js
+++ pages/subscribe.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router"
-import SubscribeButton from "../components/SubscribeButton"
+import SubscribeButton from "../src/components/SubscribeButton"
 
 export default function SubscribePage() {
   const router = useRouter()
