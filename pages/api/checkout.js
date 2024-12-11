3571809979c3aa8dbf952e22289d92fca88f93e4
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-12-11 00:15:31 +0000

    test

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,5 +1,5 @@
 import Stripe from "stripe"
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripe = new Stripe(process.env.STRIPE_TEST_KEY)
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
