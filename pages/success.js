55826ceaa3b5e224e8d88b98adb674763b75412d
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-12-12 20:49:42 +0000

    edit success page

--- pages/success.js
+++ pages/success.js
@@ -2,6 +2,10 @@
 
 import { useRouter } from "next/router"
 
+import Button from "src/components/Button"
+import Header from "src/components/Header"
+import Footer from "src/components/Footer"
+
 export default function SuccessPage() {
   const router = useRouter()
 
@@ -10,17 +14,19 @@ export default function SuccessPage() {
   }
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center px-4 py-8">
-      <h1 className="text-2xl font-bold">Thank You for Subscribing!</h1>
-      <p className="mt-4 text-lg text-gray-600">
-        Please click below to go to the Login page.
-      </p>
-      <button
-        onClick={handleRedirect}
-        className="mt-8 rounded-lg bg-blue-500 px-6 py-3 text-white hover:bg-blue-600"
-      >
-        Go to Login
-      </button>
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="flex flex-grow flex-col items-center justify-center px-6 py-8 lg:px-16 lg:py-16">
+        <div className="text-center">
+          <h1 className="text-2xl font-bold">Thank you for subscribing!</h1>
+          <p className="mt-4 text-lg text-gray-600">
+            Please click the button to go to the Login page.
+          </p>
+
+          <Button onClick={handleRedirect}>Go to Login</Button>
+        </div>
+      </main>
+      <Footer />
     </div>
   )
 }
