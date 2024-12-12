3b6f038c59f78a9a6f33174168a0585733b70838
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-12-12 22:27:20 +0000

    cancel and success

--- src/components/CancelContent.jsx
+++ src/components/CancelContent.jsx
@@ -1,11 +1,13 @@
 export default function CancelContent() {
   return (
-    <div className="py-16 text-center">
-      <h1 className="text-2xl font-bold">Subscription Canceled</h1>
-      <p className="mt-4 text-lg text-gray-600">
-        It looks like you canceled your subscription. If this was a mistake, you
-        can try again.
-      </p>
+    <div className="flex flex-grow items-center justify-center py-8 text-center">
+      <div>
+        <h2 className="text-3xl font-bold">Subscription Cancelled</h2>
+        <p className="mt-4 text-lg">
+          It looks like you canceled your subscription. If this was a mistake,
+          you can try again.
+        </p>
+      </div>
     </div>
   )
 }
--- src/components/SuccessContent.jsx
+++ src/components/SuccessContent.jsx
@@ -17,16 +17,18 @@ export default function SuccessContent() {
   }, [router])
 
   return (
-    <div className="py-16 text-center">
-      <h1 className="text-2xl font-bold">Thank you for subscribing!</h1>
-      <p className="mt-4 text-lg text-gray-600">
-        Please wait while we redirect you to the Login page.
-      </p>
-      {loading && (
-        <div className="mt-6 flex items-center justify-center">
-          <div className="h-16 w-16 animate-spin rounded-full border-4 border-blue-500 border-t-transparent"></div>
-        </div>
-      )}
+    <div className="flex flex-grow items-center justify-center py-8 text-center">
+      <div>
+        <h2 className="text-3xl font-bold">Thank you for subscribing!</h2>
+        <p className="mt-4 text-lg">
+          Please wait while we redirect you to the main page.
+        </p>
+        {loading && (
+          <div className="mt-6 flex items-center justify-center">
+            <div className="h-16 w-16 animate-spin rounded-full border-4 border-blue-500 border-t-transparent"></div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
