33e4f0910ea6f08b152196055c187952888b05fe
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-12-13 20:34:35 +0000

    fix styles

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -65,7 +65,7 @@ export default function Dropdown({ attributes, selectedValue, onChange }) {
                 onChange(attr)
                 setIsOpen(false)
               }}
-              className="cursor-pointer px-4 py-2 text-[#e8e8e8] hover:bg-gray-600 hover:text-white"
+              className="cursor-pointer px-4 py-2 text-[#e8e8e8] hover:rounded hover:bg-[#2d2c39] hover:text-white"
             >
               {attr.charAt(0).toUpperCase() + attr.slice(1)}
             </li>
