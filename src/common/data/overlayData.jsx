f3c9a138f1e7b7dc8bce71c96c46233f9eff4e43
João Francisco Reis <joaofranciscoreis13@outlook.com>
2024-12-16 12:25:05 +0000

    update image paths

--- src/common/data/overlayData.jsx
+++ src/common/data/overlayData.jsx
@@ -2,16 +2,16 @@ export const overlayInfo = [
   {
     title: "Mobility",
     image:
-      "https://expvector.s3.eu-north-1.amazonaws.com/images/mobilityBackground.webp",
+      "https://expvector.s3.eu-north-1.amazonaws.com/images/backgrounds/mobilityBackground.webp",
   },
   {
     title: "AI",
     image:
-      "https://expvector.s3.eu-north-1.amazonaws.com/images/aiBackground.webp",
+      "https://expvector.s3.eu-north-1.amazonaws.com/images/backgrounds/aiBackground.webp",
   },
   {
     title: "Fintech",
     image:
-      "https://expvector.s3.eu-north-1.amazonaws.com/images/fintechBackground.webp",
+      "https://expvector.s3.eu-north-1.amazonaws.com/images/backgrounds/fintechBackground.webp",
   },
 ]
