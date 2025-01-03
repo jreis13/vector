b08404c13c83a63ff4ea29b31e88ec8501c68b69
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-01-03 18:23:16 +0000

    fix checkout

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -6,15 +6,21 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const { subscriptions } = req.body
 
-    if (!subscriptions || subscriptions.length === 0) {
-      return res.status(400).json({ error: "No subscriptions provided." })
+    if (
+      !subscriptions ||
+      !Array.isArray(subscriptions) ||
+      subscriptions.length === 0
+    ) {
+      return res.status(400).json({ error: "Invalid subscriptions payload." })
     }
 
     try {
       const lineItems = subscriptions.map((subscription) => ({
-        price: "price_1QUdY7H8mb7EVuIwB4Y5Q87V",
-        quantity: subscription.quantity,
+        price: "price_1QUdY7H8mb7EVuIwB4Y5Q87V", // Replace with your actual price ID
+        quantity: 1,
       }))
+
+      console.log("Line items:", lineItems)
 
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
@@ -26,6 +32,8 @@ export default async function handler(req, res) {
         success_url: `${process.env.AUTH0_BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${process.env.AUTH0_BASE_URL}/cancel`,
       })
+
+      console.log("Checkout session created:", session.url)
 
       return res.status(200).json({ url: session.url })
     } catch (err) {
