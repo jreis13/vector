c29128b83a04204af56e3ef3acbb5365c26d1978
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-01-05 19:54:31 +0000

    improve stats

--- src/components/Company/CompanyStats.jsx
+++ src/components/Company/CompanyStats.jsx
@@ -1,16 +1,16 @@
 export default function CompanyStats({ stats }) {
   return (
-    <div className="grid grid-cols-1 gap-6 pb-8 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-8">
       {stats &&
         stats.map(
           (stat, index) =>
             !stat.value.includes("http") && (
               <div
                 key={index}
-                className="rounded-lg bg-[#403f4c] border-[#e8e8e8] shadow-lg p-6 transition-transform transform border"
+                className="rounded-lg bg-[#403f4c] w-full p-8 flex flex-col justify-between"
               >
-                <p className="font-semibold text-[#e8e8e8] text-lg">
-                  <span className="block text-[#b8b8b8] text-sm font-medium mb-1">
+                <p className="text-[#e8e8e8] font-semibold text-xl">
+                  <span className="block text-[#b8b8b8] text-md font-medium mb-1">
                     {stat.label}
                   </span>
                   {stat.value}
