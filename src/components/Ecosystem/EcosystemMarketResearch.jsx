d6134e995b12d7f7b405e573886d2fe340916e8b
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-01-07 22:09:15 +0000

    split routes and m research to components

---
+++ src/components/Ecosystem/EcosystemMarketResearch.jsx
@@ -0,0 +1,12 @@
+"use client"
+
+export default function EcosystemMarketResearch() {
+  return (
+    <div>
+      <h3 className="text-2xl font-bold mb-4 text-center">Coming Soon</h3>
+      <p className="font-bold text-center">
+        Information about Market Research will be available soon.
+      </p>
+    </div>
+  )
+}
---
+++ src/components/Ecosystem/EcosystemRoutes.jsx
@@ -0,0 +1,12 @@
+"use client"
+
+export default function EcosystemRoutes() {
+  return (
+    <div>
+      <h3 className="text-2xl font-bold mb-4 text-center">Coming Soon</h3>
+      <p className="font-bold text-center">
+        Information about Routes will be available soon.
+      </p>
+    </div>
+  )
+}
