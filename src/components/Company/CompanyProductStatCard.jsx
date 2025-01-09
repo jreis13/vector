0afbbfc46a37c40c6dcdfc661b27c5bad0bfa0f5
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-01-09 23:11:54 +0000

    Edit ProductStatCard

--- src/components/Company/CompanyProductStatCard.jsx
+++ src/components/Company/CompanyProductStatCard.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function CompanyProductStatCard({ product }) {
   return product.image ? (
-    <div className="relative flex flex-col bg-white rounded-lg border overflow-hidden shadow-lg transition-all duration-300 hover:shadow-xl w-[700px] min-h-[400px]">
+    <div className="relative flex bg-[#34333d] flex-col rounded-lg overflow-hidden shadow-lg transition-all duration-300 hover:shadow-xl w-[700px] min-h-[400px]">
       <div className="absolute inset-0">
         <Image
           src={product.image}
@@ -12,12 +12,27 @@ export default function CompanyProductStatCard({ product }) {
           objectPosition="center"
           priority
         />
-        <div className="absolute inset-0 bg-black bg-opacity-40"></div>
       </div>
       <div className="relative z-10 flex flex-col justify-between p-6 text-white">
         <div>
-          <h3 className="text-2xl font-bold mb-4">{product.name}</h3>
-          <p className="mb-6">{product.description}</p>
+          <h3
+            className="text-2xl font-bold mb-4"
+            style={{
+              textShadow:
+                "1px 1px 1px #34333d, 0px 0px 1px #34333d, 1px 0px 1px #34333d, 0px 1px 1px #34333d",
+            }}
+          >
+            {product.name}
+          </h3>
+          <p
+            className="mb-6"
+            style={{
+              textShadow:
+                "1px 1px 1px #34333d, 0px 0px 1px #34333d, 1px 0px 1px #34333d, 0px 1px 1px #34333d",
+            }}
+          >
+            {product.description}
+          </p>
         </div>
         <div className="w-full border-t border-white pt-4">
           <div className="grid grid-cols-2 gap-x-6 gap-y-4">
@@ -26,7 +41,14 @@ export default function CompanyProductStatCard({ product }) {
                 key !== "name" &&
                 key !== "description" &&
                 key !== "image" && (
-                  <div key={key} className="flex flex-col">
+                  <div
+                    key={key}
+                    className="flex flex-col"
+                    style={{
+                      textShadow:
+                        "1px 1px 1px #34333d, 0px 0px 1px #34333d, 1px 0px 1px #34333d, 0px 1px 1px #34333d",
+                    }}
+                  >
                     <span className="font-medium capitalize">
                       {key.replace(/_/g, " ")}:
                     </span>
