8608b69ee31bb92d4d6ef43367818eb95c75072a
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-01-09 23:48:57 +0000

    Adjust investor cards

--- src/components/InvestorCard.jsx
+++ src/components/InvestorCard.jsx
@@ -2,19 +2,35 @@ import Image from "next/image"
 
 export default function InvestorCard({ investor }) {
   return (
-    <div className="flex h-[200px] flex-col items-center justify-center p-4">
-      <div className="h-16 w-16 mb-4 flex items-center justify-center overflow-hidden">
-        <Image
-          src={investor.logo}
-          alt={`${investor.name} logo`}
-          width={64}
-          height={64}
-          style={{ objectFit: "cover" }}
-        />
+    <div className="flex flex-col justify-between rounded-lg p-4 bg-[#34333d] text-[#e8e8e8]">
+      <div>
+        <div className="mb-4 flex items-center">
+          <div className="mr-4 flex h-16 w-16 items-center justify-center overflow-hidden">
+            <Image
+              src={investor.logo}
+              alt={`${investor.name} logo`}
+              width={64}
+              height={64}
+              style={{ objectFit: "contain" }}
+            />
+          </div>
+          <div>
+            <h3 className="text-xl font-bold">{investor.name}</h3>
+          </div>
+        </div>
+        <div className="mb-4">
+          <p>{investor.summary}</p>
+        </div>
       </div>
-      <p className="text-center text-lg font-semibold text-white">
-        {investor.name}
-      </p>
+      <div className="mt-4 flex flex-col justify-end">
+        {investor.mainStats &&
+          investor.mainStats.map((stat, index) => (
+            <div key={index} className="flex justify-between py-1">
+              <span>{stat.label}:</span>
+              <span className="text-right">{stat.value}</span>
+            </div>
+          ))}
+      </div>
     </div>
   )
 }
