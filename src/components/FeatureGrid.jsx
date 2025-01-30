9719cb528b57014e1e6c216b869af6ae9f20b5d4
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-01-30 01:00:20 +0000

    adjust feature grid issues

--- src/components/FeatureGrid.jsx
+++ src/components/FeatureGrid.jsx
@@ -29,9 +29,9 @@ export default function FeatureGrid({ title, features }) {
           {features?.map((feature, index) => (
             <div
               key={index}
-              className="p-6 rounded-xl flex flex-col items-center text-center bg-[#34333d]"
+              className="p-6 rounded-xl flex flex-col items-center text-center bg-[#34333d] h-full"
             >
-              <HoverScale className="h-full w-full flex flex-col items-center justify-center">
+              <HoverScale className="h-full w-full flex flex-col justify-between items-center">
                 <div className="flex flex-col items-center">
                   {feature.name && (
                     <h3 className="py-4 mb-2 text-lg md:text-xl lg:text-2xl font-semibold">
@@ -46,13 +46,13 @@ export default function FeatureGrid({ title, features }) {
                 </div>
                 {feature.image && (
                   <div className="flex items-center justify-center">
-                    <div className="relative mt-4 h-40 w-40 lg:h-64 lg:w-64">
+                    <div className="relative h-40 md:h-48 lg:h-64 w-full flex justify-center">
                       <Image
                         src={feature.image}
                         alt={feature.name || feature.description}
-                        fill
-                        sizes="100vw"
-                        style={{ objectFit: "contain" }}
+                        className="h-full object-contain"
+                        width={256}
+                        height={256}
                       />
                     </div>
                   </div>
