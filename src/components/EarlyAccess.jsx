6ffd644314f3d4818e840fef1913bb37251578d6
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-01-31 13:29:10 +0000

    adjust size od EarlyAccess label

--- src/components/EarlyAccess.jsx
+++ src/components/EarlyAccess.jsx
@@ -1,7 +1,7 @@
 export default function EarlyAccess() {
   return (
     <div className="m-10 relative">
-      <div className="absolute top-8 right-0 bg-[#d87103] text-[#403f4c] px-4 py-2 rounded-lg text-sm font-semibold shadow-md">
+      <div className="absolute top-8 right-0 bg-[#d87103] text-[#403f4c] px-4 py-2 rounded-lg text-md font-semibold shadow-md">
         Now in Early Access
       </div>
     </div>
