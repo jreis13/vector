689d45a8190bd5d9652ef167d869823f690f53bb
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-02-03 01:33:01 +0000

    change dynamic list card

--- src/components/Ecosystem/StatCards/DynamicListCard.jsx
+++ src/components/Ecosystem/StatCards/DynamicListCard.jsx
@@ -7,14 +7,12 @@ import PercentageChart from "./PercentageChart"
 export default function DynamicListCard({ data }) {
   const renderContent = (item, index) => {
     if (typeof item === "object" && item.type === "percentage") {
-      const percentage = item.percentage
-
       return (
         <div
           key={index}
           className="flex flex-col items-center text-center p-4 gap-4 h-fit"
         >
-          <PercentageChart percentage={percentage} />
+          <PercentageChart percentage={item.percentage} />
           <div className="flex flex-col gap-2">
             <p className="font-bold text-lg">{item.subtitle}</p>
             <p>{item.description}</p>
@@ -22,14 +20,12 @@ export default function DynamicListCard({ data }) {
         </div>
       )
     } else if (typeof item === "object" && item.type === "number") {
-      const iconSrc = icons[item.icon]
-
       return (
         <div
           key={index}
           className="flex flex-col items-center text-center p-4 h-fit"
         >
-          <div className="flex items-center justify-center text-4xl mb-2">
+          <div className="flex flex-col items-center justify-center text-4xl mb-2">
             {icons[item.icon] ? (
               <Image
                 src={icons[item.icon]}
@@ -48,43 +44,39 @@ export default function DynamicListCard({ data }) {
               />
             ) : null}
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2 text-center">
             <p className="font-bold text-lg">{item.subtitle}</p>
-            <p className="text-3xl number">{item.description}</p>
+            <p className="text-3xl">{item.description}</p>
           </div>
         </div>
       )
     } else if (typeof item === "object" && item.subtitle) {
-      const iconSrc = icons[item.icon]
-
       return (
         <div
           key={index}
-          className="flex flex-col items-center text-center py-4 h-fit w-fit"
+          className="flex flex-col items-center text-center p-4 w-full"
         >
-          <div className="flex items-center justify-center text-4xl mb-2">
+          <div className="flex flex-col items-center justify-center text-4xl mb-2">
             {icons[item.icon] ? (
               <Image
-                src={icons[item.icon]}
+                src={icons[item.icon] || "/public/icons/defaultIcon.svg"}
                 alt={item.subtitle || "icon"}
-                width={80}
-                height={80}
-                className="w-20 h-20 object-contain"
+                width={50}
+                height={50}
               />
             ) : item.logo ? (
               <Image
                 src={item.logo}
                 alt={item.subtitle}
-                width={80}
-                height={80}
-                className="w-20 h-20 object-contain"
+                width={50}
+                height={50}
               />
             ) : null}
           </div>
-          <div className="flex flex-col gap-2">
-            <p className="font-bold text-xl">{item.subtitle}</p>
-            <p>{item.description || ""}</p>
-          </div>
+          <h3 className="font-bold text-xl">{item.subtitle}</h3>
+          {item.description && (
+            <p className="text-sm text-gray-400 mt-2">{item.description}</p>
+          )}
         </div>
       )
     } else if (Array.isArray(item)) {
@@ -96,17 +88,15 @@ export default function DynamicListCard({ data }) {
         </ul>
       )
     } else if (typeof item === "string") {
-      const iconSrc = icons[data.icon]
-
       return (
         <li
-          className="flex flex-col items-center text-center py-4 h-fit w-fit"
           key={index}
+          className="flex flex-col items-center text-center py-4 h-fit w-fit"
         >
-          <div className="text-4xl text-[#6600cc]">
-            {iconSrc ? (
+          <div className="flex flex-col items-center text-4xl text-[#6600cc]">
+            {icons[data.icon] ? (
               <Image
-                src={iconSrc}
+                src={icons[data.icon]}
                 alt={item}
                 width={50}
                 height={50}
@@ -131,7 +121,7 @@ export default function DynamicListCard({ data }) {
   }
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 justify-start text-center">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-center">
       {Array.isArray(data.value)
         ? data.value.map((item, index) => renderContent(item, index))
         : renderContent(data.value, 0)}
