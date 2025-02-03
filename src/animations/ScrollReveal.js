4b43cdec85da7b86a6658ebe98011ec5e9bc18cd
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-02-03 01:40:02 +0000

    fix scroll reveal

--- src/animations/ScrollReveal.js
+++ src/animations/ScrollReveal.js
@@ -1,13 +1,16 @@
 import { motion } from "framer-motion"
 
-const ScrollReveal = ({ children, className, ...props }) => {
+const ScrollReveal = ({ children, className, isFirst, isLast, ...props }) => {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 50 }}
+      initial={{ opacity: 0, y: isFirst ? 0 : 50 }} // Prevents first item from animating upwards
       whileInView={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: 50 }}
+      exit={isLast ? {} : { opacity: 0, y: 50 }} // Prevent hiding the last item
       transition={{ duration: 0.8 }}
-      viewport={{ once: false, amount: 0.4 }}
+      viewport={{
+        once: isFirst || isLast ? true : false,
+        amount: isLast || isFirst ? 0.1 : 0.4,
+      }} // Ensures first & last items stay visible
       className={`p-8 ${className}`}
       {...props}
     >
--- src/components/Ecosystem/EcosystemCountryProfilesDetails.jsx
+++ src/components/Ecosystem/EcosystemCountryProfilesDetails.jsx
@@ -34,172 +34,20 @@ export default function EcosystemCountryProfilesDetails({
       <h1 className="text-4xl font-bold mb-16">{countryName}</h1>
 
       <div className="space-y-16">
-        {reports.map((report, reportIndex) =>
-          reportIndex === 0 ? (
-            <div key={reportIndex} id={breadcrumbSections[reportIndex].id}>
-              <h2 className="mb-8">{report.title}</h2>
-              {hasHtmlType(report.details) ? (
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-start">
-                  <div className="space-y-6">
-                    {Object.entries(report.details)
-                      .filter(([, value]) => value.type !== "html")
-                      .map(([key, value], idx, arr) => (
-                        <div
-                          key={idx}
-                          className={`relative flex flex-col h-full items-start gap-4 p-4 bg-[#34333d] rounded-lg ${
-                            arr.length % 2 !== 0 && idx === arr.length - 1
-                              ? "col-span-full"
-                              : ""
-                          }`}
-                        >
-                          <div className="flex items-center gap-4">
-                            <div className="flex-shrink-0">
-                              {value.icon ? (
-                                <Image
-                                  src={icons[value.icon]}
-                                  alt={value.subtitle || "icon"}
-                                  width={40}
-                                  height={40}
-                                />
-                              ) : value.logo ? (
-                                <Image
-                                  src={value.logo}
-                                  alt={value.subtitle}
-                                  width={40}
-                                  height={40}
-                                />
-                              ) : null}
-                            </div>
-                            <h3 className="mb-2 text-3xl ml-6">{key}</h3>
-                          </div>
-                          <div className="w-full">
-                            {value.type === "list" ||
-                            value.type === "nested-list" ? (
-                              <DynamicListCard data={value} />
-                            ) : (
-                              <InfoCard data={value} />
-                            )}
-                          </div>
-                          {value.source && (
-                            <div className="absolute top-4 right-4 group">
-                              <Image
-                                src={icons.infoIcon}
-                                alt="source icon"
-                                width={20}
-                                height={20}
-                                className="cursor-pointer"
-                              />
-                              <div className="absolute hidden group-hover:block bg-black text-white text-sm rounded-lg p-2 w-52 z-10">
-                                <p>{value.source}</p>
-                              </div>
-                            </div>
-                          )}
-                        </div>
-                      ))}
-                  </div>
+        {reports.map((report, reportIndex) => {
+          const isFirst = reportIndex === 0
+          const isLast = reportIndex === reports.length - 1
 
-                  <div className="relative">
-                    {Object.entries(report.details)
-                      .filter(([, value]) => value.type === "html")
-                      .map(([, value], idx) => (
-                        <iframe
-                          key={idx}
-                          className="w-full h-[calc(80vh-100px)] border-none px-4 bg-[#34333d] rounded-lg"
-                          src={value.value}
-                          allowFullScreen
-                        ></iframe>
-                      ))}
-                  </div>
-                </div>
-              ) : (
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-start">
-                  {Object.entries(report.details)
-                    .filter(([, value]) => value.type !== "html")
-                    .map(([key, value], idx, arr) => (
-                      <div
-                        key={idx}
-                        className={`relative flex flex-col h-full items-start gap-4 p-4 bg-[#34333d] rounded-lg ${
-                          idx === arr.length - 1 ? "col-span-full" : ""
-                        }`}
-                      >
-                        <div className="flex items-center gap-4">
-                          <div className="flex-shrink-0">
-                            {value.icon ? (
-                              <Image
-                                src={icons[value.icon]}
-                                alt={value.subtitle || "icon"}
-                                width={80}
-                                height={80}
-                                className="w-20 h-20 object-contain"
-                              />
-                            ) : value.logo ? (
-                              <Image
-                                src={value.logo}
-                                alt={value.subtitle}
-                                width={80}
-                                height={80}
-                                className="w-20 h-20 object-contain"
-                              />
-                            ) : null}
-                          </div>
-                          <h3 className="mb-2 text-3xl mr-6">{key}</h3>
-                        </div>
-                        <div className="w-full">
-                          {(() => {
-                            switch (value.type) {
-                              case "list":
-                              case "nested-list":
-                                return <DynamicListCard data={value} />
-                              case "html":
-                                return (
-                                  <div className="relative">
-                                    <iframe
-                                      className="w-full h-full border-none"
-                                      src={value.value}
-                                      allowFullScreen
-                                    ></iframe>
-                                  </div>
-                                )
-                              case "number":
-                                return <InfoCard data={value} />
-                              case "table":
-                                return (
-                                  <DynamicTransportTable
-                                    reports={reports}
-                                    countryName={countryName}
-                                  />
-                                )
-                              default:
-                                return <InfoCard data={value} />
-                            }
-                          })()}
-                        </div>
-                        {value.source && (
-                          <div className="absolute top-4 right-4 group">
-                            <Image
-                              src={icons.infoIcon}
-                              alt="source icon"
-                              width={30}
-                              height={30}
-                              className="cursor-pointer"
-                            />
-                            <div className="absolute hidden group-hover:block bg-[#e8e8e8] text-[#34333d] text-sm rounded-lg mt-2 p-2 w-40 z-10">
-                              <p>{value.source}</p>
-                            </div>
-                          </div>
-                        )}
-                      </div>
-                    ))}
-                </div>
-              )}
-            </div>
-          ) : (
+          return (
             <ScrollReveal
               key={reportIndex}
               id={breadcrumbSections[reportIndex].id}
+              isFirst={isFirst}
+              isLast={isLast}
             >
-              <div key={reportIndex} id={breadcrumbSections[reportIndex].id}>
+              <div id={breadcrumbSections[reportIndex].id}>
                 <h2 className="mb-8">{report.title}</h2>
+
                 {hasHtmlType(report.details) ? (
                   <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-start">
                     <div className="space-y-6">
@@ -240,34 +88,7 @@ export default function EcosystemCountryProfilesDetails({
                                 <InfoCard data={value} />
                               )}
                             </div>
-                            {value.source && (
-                              <div className="absolute top-4 right-4 group">
-                                <Image
-                                  src={icons.infoIcon}
-                                  alt="source icon"
-                                  width={20}
-                                  height={20}
-                                  className="cursor-pointer"
-                                />
-                                <div className="absolute hidden group-hover:block bg-black text-white text-sm rounded-lg p-2 w-52 z-10">
-                                  <p>{value.source}</p>
-                                </div>
-                              </div>
-                            )}
                           </div>
-                        ))}
-                    </div>
-
-                    <div className="relative">
-                      {Object.entries(report.details)
-                        .filter(([, value]) => value.type === "html")
-                        .map(([, value], idx) => (
-                          <iframe
-                            key={idx}
-                            className="w-full h-[calc(80vh-100px)] border-none px-4 bg-[#34333d] rounded-lg"
-                            src={value.value}
-                            allowFullScreen
-                          ></iframe>
                         ))}
                     </div>
                   </div>
@@ -331,26 +152,11 @@ export default function EcosystemCountryProfilesDetails({
                                       countryName={countryName}
                                     />
                                   )
-
                                 default:
                                   return <InfoCard data={value} />
                               }
                             })()}
                           </div>
-                          {value.source && (
-                            <div className="absolute top-4 right-4 group">
-                              <Image
-                                src={icons.infoIcon}
-                                alt="source icon"
-                                width={30}
-                                height={30}
-                                className="cursor-pointer"
-                              />
-                              <div className="absolute hidden group-hover:block bg-[#e8e8e8] text-[#34333d] text-sm rounded-lg mt-2 p-2 w-40 z-10">
-                                <p>{value.source}</p>
-                              </div>
-                            </div>
-                          )}
                         </div>
                       ))}
                   </div>
@@ -358,7 +164,7 @@ export default function EcosystemCountryProfilesDetails({
               </div>
             </ScrollReveal>
           )
-        )}
+        })}
       </div>
 
       <button
