c706813e7d01bb913468f0c4981dfd16f2c70d7b
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-02-04 20:17:35 +0000

    early access  mobile changes

--- src/components/EarlyAccess.jsx
+++ src/components/EarlyAccess.jsx
@@ -1,7 +1,7 @@
 export default function EarlyAccess() {
   return (
-    <div className="m-10 relative">
-      <div className="absolute top-8 right-0 bg-[#d87103] text-[#403f4c] px-4 py-2 rounded-lg text-md font-semibold shadow-md">
+    <div className="relative lg:m-10">
+      <div className="absolute top-[-4rem] left-0 w-full bg-[#d87103] text-[#403f4c] px-6 py-3 rounded-lg text-md font-semibold shadow-md text-center lg:w-auto lg:top-8 lg:right-0 lg:left-auto lg:translate-x-0 lg:px-4 lg:py-2">
         Now in Early Access
       </div>
     </div>
