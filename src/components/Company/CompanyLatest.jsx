6903e61bbc51eb980daf8fb0719ac69585ff3cee
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-02-05 23:27:00 +0000

    fallback latest developments

--- src/components/Company/CompanyLatest.jsx
+++ src/components/Company/CompanyLatest.jsx
@@ -6,9 +6,9 @@ export default function CompanyLatest({ title, data }) {
     <div className="flex flex-col py-8 lg:py-16">
       <h2 className="mb-8">{title}</h2>
       <div className="w-full bg-[#34333d] rounded-lg p-6 shadow-lg">
-        <ul className="list-disc list-inside space-y-4">
-          {data &&
-            data.map((item, index) => (
+        {data && data.length > 0 ? (
+          <ul className="list-disc list-inside space-y-4">
+            {data.map((item, index) => (
               <li key={index} className="flex items-start space-x-4">
                 <FontAwesomeIcon
                   icon={faCalendarAlt}
@@ -22,7 +22,12 @@ export default function CompanyLatest({ title, data }) {
                 </div>
               </li>
             ))}
-        </ul>
+          </ul>
+        ) : (
+          <p className="text-[#b8b8b8] text-lg text-center">
+            No developments to report at the moment.
+          </p>
+        )}
       </div>
     </div>
   )
