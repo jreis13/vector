2654a79174e68dd0bea15534fb8876680d31699c
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-02-26 13:53:44 +0000

    adjust infoCard

--- src/components/Ecosystem/StatCards/InfoCard.jsx
+++ src/components/Ecosystem/StatCards/InfoCard.jsx
@@ -3,29 +3,33 @@
 export default function InfoCard({ data }) {
   let label = data.value || ""
   let description = ""
+
   if (label.includes(" - ")) {
     const splitIndex = label.indexOf(" - ")
-    label = label.substring(0, splitIndex).trim()
     description = label.substring(splitIndex + 2).trim()
+    label = label.substring(0, splitIndex).trim()
   }
+
   const subtitle = data.subtitle || ""
   const unit = data.unit || ""
   const notes = data.notes || ""
 
+  const numericLabel = label.replace(/,/g, "")
+  const isNumber = !isNaN(numericLabel) && numericLabel.trim() !== ""
+
   return (
     <div className="px-2">
-      {subtitle && (
-        <p className="text-lg font-semibold text-gray-300">{subtitle}</p>
-      )}
+      {subtitle && <p className="font-semibold">{subtitle}</p>}
 
-      <div className="flex gap-1 items-center">
-        <p className="text-2xl font-bold">{label}</p>
-        {description && <p className="text-[#b8b8b8]">{description}</p>}
+      <div className="flex items-end gap-1">
+        <p className={`text-3xl ${isNumber ? "number" : ""}`}>{label}</p>
         {unit && <p className="text-2xl">{unit}</p>}
       </div>
 
+      {description && <p className="text-[#b8b8b8]">{description}</p>}
+
       {notes && (
-        <p className="absolute bottom-2 right-2 text-xs text-gray-400">
+        <p className="absolute bottom-2 right-2 text-xs text-[#b8b8b8]">
           Info collected from: {notes}
         </p>
       )}
