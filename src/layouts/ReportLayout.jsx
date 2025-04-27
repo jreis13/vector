aa562d033c5fec5643cccba3a99e43cc6f2937e3
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-04-27 14:30:10 +0100

    remove header and footer from ReportLayout to force full screen

--- src/layouts/ReportLayout.jsx
+++ src/layouts/ReportLayout.jsx
@@ -1,17 +1,13 @@
 import Report from "src/components/Report"
-import Footer from "src/components/Structure/Footer"
-import Header from "src/components/Structure/Header"
 
 export default function ReportLayout({ reportId }) {
   return (
     <div className="flex min-h-screen flex-col">
-      <Header />
       <div className="flex flex-grow flex-col">
         <div id="Reports" className="flex-grow">
           <Report reportId={reportId} />
         </div>
       </div>
-      <Footer />
     </div>
   )
 }
