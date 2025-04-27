b02643004deb182400a3790ac2b6456602c1c422
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-04-27 15:29:09 +0100

    update reports

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -1,20 +1,15 @@
 "use client"
 
 import { useUser } from "@auth0/nextjs-auth0/client"
+import Image from "next/image"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 import LoadingLayout from "src/layouts/LoadingLayout"
+import Button from "./Button"
 
-const FREE_REPORT_ID = "advancedairmobility_q1_2025"
+import { CheckIcon } from "@heroicons/react/24/outline"
 
-const REPORT_DISPLAY = {
-  advancedairmobility_q1_2025: {
-    title:
-      "AAM - Global eVTOL Market Review (Passenger Commercial Tracker) - Q1 2025",
-    image:
-      "https://expvector.s3.eu-north-1.amazonaws.com/images/reportCovers/advancedairmobility_q1_2025_cover.png",
-  },
-}
+import { FREE_REPORT_ID, REPORT_DISPLAY } from "src/common/data/reportData"
 
 export default function Reports() {
   const { user, isLoading } = useUser()
@@ -36,24 +31,55 @@ export default function Reports() {
   return (
     <div className="p-8">
       <h1 className="text-3xl font-semibold mb-6">Reports</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {reports.map((id) => (
-          <div
-            key={id}
-            className="relative h-60 rounded-xl shadow-md hover:shadow-lg cursor-pointer overflow-hidden group"
-            onClick={() => window.open(`/reports/${id}`, "_blank")}
-          >
+      <div className="flex gap-6">
+        {reports.map((id) => {
+          const report = REPORT_DISPLAY[id]
+
+          return (
             <div
-              className="absolute inset-0 bg-cover transition-transform scale-100 group-hover:scale-105 duration-300"
-              style={{ backgroundImage: `url(${REPORT_DISPLAY[id]?.image})` }}
-            />
-            <div className="absolute inset-0 bg-black bg-opacity-60 p-6 flex flex-col justify-end">
-              <h2 className="text-xl font-bold text-white">
-                {REPORT_DISPLAY[id]?.title || id}
-              </h2>
+              key={id}
+              className="rounded-xl overflow-hidden bg-[#1a1a1a] group transition-all duration-300"
+            >
+              <div className="w-full overflow-hidden">
+                <Image
+                  src={report?.image}
+                  alt={report?.title}
+                  width={800}
+                  height={800}
+                  className="w-full max-h-full object-fit transition-transform duration-300 bg:[#34333d] hover:bg-[#1a1a1a] group-hover:opacity-70"
+                />
+              </div>
+
+              <div className="p-4">
+                <h2 className="text-lg font-bold mb-2">
+                  {report?.title || id}
+                </h2>
+              </div>
+
+              <div className="px-4 max-h-0 opacity-0 overflow-hidden group-hover:max-h-[400px] group-hover:opacity-100 transition-all duration-500 ease-in-out">
+                <ul className="text-md space-y-2 pb-4">
+                  {(report?.features || []).map((feature, index) => (
+                    <li key={index} className="flex items-center gap-2">
+                      <CheckIcon className="h-4 w-4 text-white" />
+                      <span>{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+                <div className="flex justify-center gap-4 mb-4">
+                  <Button
+                    alternative
+                    onClick={() => window.open(`/reports/${id}`, "_blank")}
+                  >
+                    Open Free Report
+                  </Button>
+                  <Button onClick={() => (window.location.href = "/subscribe")}>
+                    Buy Full Report
+                  </Button>
+                </div>
+              </div>
             </div>
-          </div>
-        ))}
+          )
+        })}
       </div>
     </div>
   )
