9b7965de677892e47c97a379ad7a1c1faf592d77
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-04-27 15:29:16 +0100

    add alternative button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,6 +6,7 @@ export default function Button({
   onClick,
   href,
   children,
+  alternative,
   blank = false,
   disabled = false,
 }) {
@@ -14,13 +15,17 @@ export default function Button({
 
   const enabledHoverStyles = "hover:bg-[#6600cc] hover:text-[#e8e8e8]"
   const disabledStyles = "opacity-50 cursor-not-allowed"
+  const alternativeStyles =
+    "bg-[#e8e8e8] text-[#34333d] hover:bg-[#6600cc] hover:text-[#e8e8e8]"
 
   if (onClick) {
     return (
       <motion.button
         whileHover={!disabled ? { scale: 1.05 } : {}}
         whileTap={!disabled ? { scale: 0.95 } : {}}
-        className={`${baseStyles} ${disabled ? disabledStyles : enabledHoverStyles}`}
+        className={`${baseStyles} ${disabled ? disabledStyles : enabledHoverStyles} ${
+          alternative ? alternativeStyles : ""
+        }`}
         onClick={disabled ? undefined : onClick}
         type="button"
         disabled={disabled}
