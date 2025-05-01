1308338ad247c6549d2406551997b2f814e1a7bb
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-05-01 19:52:46 +0100

    remove features from free report

--- src/common/data/reportData.jsx
+++ src/common/data/reportData.jsx
@@ -7,13 +7,7 @@ export const REPORT_DISPLAY = {
       "AAM - Global eVTOL Market Review (Passenger Commercial Tracker) - Q1 2025 - Free Sample",
     image:
       "https://expvector.s3.eu-north-1.amazonaws.com/images/reportCovers/advancedairmobility_q1_2025_free_cover.png",
-    features: [
-      "Deep dive on eVTOL commercial transactions (94 commercial transactions)",
-      "27 Manufacturer Profiles",
-      "32 Aircraft Analysis",
-      "3 Key Regions (North America, Europe and Asia)",
-      "+100 pages featuring insights and data analysis",
-    ],
+    features: ["20 sample slides from the full report"],
   },
   global_evtol_market_review_commercial_tracker_q1_2025: {
     title:
@@ -23,9 +17,9 @@ export const REPORT_DISPLAY = {
     features: [
       "Deep dive on eVTOL commercial transactions (94 commercial transactions)",
       "27 Manufacturer Profiles",
-      "32 Aircraft Analysis",
+      "31 Aircraft Analysis",
       "3 Key Regions (North America, Europe and Asia)",
-      "+100 pages featuring insights and data analysis",
+      "+99 pages featuring insights and data analysis",
     ],
   },
 }
