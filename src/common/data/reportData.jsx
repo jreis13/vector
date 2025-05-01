306fb72678518f31d283d96245116e798780ea18
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-05-01 16:17:32 +0100

    add data for paid report

--- src/common/data/reportData.jsx
+++ src/common/data/reportData.jsx
@@ -15,4 +15,17 @@ export const REPORT_DISPLAY = {
       "+100 pages featuring insights and data analysis",
     ],
   },
+  global_evtol_market_review_commercial_tracker_q1_2025: {
+    title:
+      "AAM - Global eVTOL Market Review (Passenger Commercial Tracker) - Q1 2025",
+    image:
+      "https://expvector.s3.eu-north-1.amazonaws.com/images/reportCovers/advancedairmobility_q1_2025_cover.png",
+    features: [
+      "Deep dive on eVTOL commercial transactions (94 commercial transactions)",
+      "27 Manufacturer Profiles",
+      "32 Aircraft Analysis",
+      "3 Key Regions (North America, Europe and Asia)",
+      "+100 pages featuring insights and data analysis",
+    ],
+  },
 }
