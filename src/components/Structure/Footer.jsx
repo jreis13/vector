5dfcc7bb30cc6f511aea2cbdd3c3aa1f11b1988f
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-05-12 15:26:15 +0100

    fix footer in mobile

--- src/components/Structure/Footer.jsx
+++ src/components/Structure/Footer.jsx
@@ -11,7 +11,7 @@ export default function Footer() {
   return (
     <footer className="bg-[#34333d] px-8 py-8 w-screen overflow-x-hidden">
       <div className="container mx-auto">
-        <div className="grid lg:grid-cols-3 grid-cols-1 gap-6 items-center">
+        <div className="grid  grid-cols-1 lg:flex gap-6 items-center">
           <div className="flex justify-center lg:justify-start">
             <Logo />
           </div>
@@ -22,7 +22,7 @@ export default function Footer() {
             </Typography>
           </div>
 
-          <div className="flex flex-row items-center justify-center lg:justify-end gap-6">
+          <div className="grid grid-cols-2 lg:flex lg:flex-row items-center justify-center lg:justify-end gap-6">
             <a
               href="/files/Privacy_Policy_Exponential_Vector.pdf"
               target="_blank"
@@ -39,27 +39,26 @@ export default function Footer() {
             >
               Terms & Conditions
             </a>
-
-            <div className="flex gap-3">
-              {socialPATHS.map(({ name, path, icon }) => (
-                <a
-                  key={name}
-                  href={path}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <IconButton variant="text" size="lg">
-                    <Image
-                      src={icon}
-                      alt={name}
-                      width={24}
-                      height={24}
-                      className="opacity-100"
-                    />
-                  </IconButton>
-                </a>
-              ))}
-            </div>
+          </div>
+          <div className="flex justify-center gap-3">
+            {socialPATHS.map(({ name, path, icon }) => (
+              <a
+                key={name}
+                href={path}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <IconButton variant="text" size="lg">
+                  <Image
+                    src={icon}
+                    alt={name}
+                    width={24}
+                    height={24}
+                    className="opacity-100"
+                  />
+                </IconButton>
+              </a>
+            ))}
           </div>
         </div>
       </div>
