aea499e3463b48200ab05111400826f0deb84d6f
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-07-29 21:50:58 +0100

    add SimulationsLayout

---
+++ src/app/simulations/page.js
@@ -0,0 +1,11 @@
+import "src/common/styles/_reset.css"
+
+import SimulationsLayout from "src/layouts/SimulationsLayout"
+
+export default function Home() {
+  return (
+    <div>
+      <SimulationsLayout />
+    </div>
+  )
+}
