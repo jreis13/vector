1a07283bad7b4449afb5c37c44d44f8a9e6be392
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-10-23 15:14:32 +0100

    remove Navigate section

--- src/common/data/breadcrumbData.jsx
+++ src/common/data/breadcrumbData.jsx
@@ -1,5 +1,4 @@
 import HomeIcon from "/public/icons/homeIcon.svg"
-import NavigateIcon from "/public/icons/navigateIcon.svg"
 import OfferingIcon from "/public/icons/offeringIcon.svg"
 import ProductIcon from "/public/icons/productIcon.svg"
 
@@ -7,5 +6,4 @@ export const breadcrumbSections = [
   { title: "Home", icon: HomeIcon, id: "" },
   { title: "Offering", icon: OfferingIcon, id: "offering" },
   { title: "Customers", icon: ProductIcon, id: "customers" },
-  { title: "Navigate", icon: NavigateIcon, id: "portal" },
 ]
